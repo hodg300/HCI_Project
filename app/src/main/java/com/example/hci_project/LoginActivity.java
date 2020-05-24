4b24481b247f16459073ca64fdde92d8fa24c177
Sarel <sarelmicha93@gmail.com>
2020-05-24 13:47:51 +0300

    *style of app has been changed *moving background

--- app/src/main/java/com/example/hci_project/LoginActivity.java
+++ app/src/main/java/com/example/hci_project/LoginActivity.java
@@ -2,23 +2,77 @@ package com.example.hci_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
+    private static int count = 0;
+    private boolean abort;
     private Button loginButton;
     private Spinner rolePicker;
+    private int[] backgrounds = {R.drawable.background1,R.drawable.background2,R.drawable.background3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        changeBackgroundImage();
         setIds();
         setSpinner();
 
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        abort = false;
+        this.changeBackground();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        abort = true;
+
+    }
+
+
+    private void changeBackground(){
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+
+                if (abort)
+                    return;
+
+                if(count< backgrounds.length - 1){
+                    count++;
+                }
+                else{
+                    count = 0;
+                }
+                changeBackgroundImage();
+                changeBackground();
+
+            }
+        }, 3000);
+    }
+
+    private void changeBackgroundImage(){
+        if (abort)
+            return;
+
+        Animation fadeIn = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.fade_out);
+        LoginActivity.this.getWindow().
+                setBackgroundDrawableResource(backgrounds[count]);
     }
 
     private void setSpinner() {
