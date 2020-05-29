646c605a95f063019a600301a1859a3c16aaaa0a
Sarel <sarelmicha93@gmail.com>
2020-05-29 12:11:51 +0300

    *Restaurant  Fragment has been added nicely

--- app/src/main/java/com/example/hci_project/MapActivity.java
+++ app/src/main/java/com/example/hci_project/MapActivity.java
@@ -2,13 +2,16 @@ package com.example.hci_project;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -51,33 +54,50 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         googleMap.setOnMarkerClickListener(this);
 
         //aHug Hatzfoni
-        markers.add(map.addMarker(new MarkerOptions().position(locations.get(0)).title("החוג הצפוני")));
+        markers.add(map.addMarker(new MarkerOptions().position(locations.get(0)).title("החוג הצפוני").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))));
         //Segev Expresss
         markers.add(map.addMarker(new MarkerOptions().position(locations.get(1)).title("שגב אקספרס")));
         //Susu and Sons
-        markers.add(map.addMarker(new MarkerOptions().position(locations.get(1)).title("סוסו אנד סאן")));
+        markers.add(map.addMarker(new MarkerOptions().position(locations.get(1)).title("סוסו אנד סאן").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))));
         //Tel Aviv musium
         markers.add(map.addMarker(new MarkerOptions().position(locations.get(1)).title("מוזיאון תל אביב")));
 
 
         //Move Camera to Segev EXPRESS
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(10);
         map.moveCamera(CameraUpdateFactory.newLatLng(locations.get(1)));
+        map.animateCamera(zoom);
+
 
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
 
-        if (markers.get(0).equals(marker)){
 
+        if (markers.get(0).equals(marker)){
+            //החוג הצפוני
+            openRestaurantDialog(marker.getTitle(),R.drawable.ahug_hatsfoni_img,126,false);
         } else if(markers.get(1).equals(marker)){
+            //שגב אקספרס
+            openRestaurantDialog(marker.getTitle(),R.drawable.segev_express_img,250,true);
 
         } else if(markers.get(2).equals(marker)){
+            //סוסו אנד סאנס
+            openRestaurantDialog(marker.getTitle(),R.drawable.susu_and_sons_img,121,false);
 
         }else if(markers.get(3).equals(marker)){
+            //מוזיאון תל אביב
+            openRestaurantDialog(marker.getTitle(),R.drawable.tel_aviv_museum,500,true);
 
         }
 
         return true;
+    }
+
+    private void openRestaurantDialog(String nameOfRestaurant, int img, int visitors,boolean isFull){
+
+        RestaurantDialog dialog = new RestaurantDialog(nameOfRestaurant,img,visitors,isFull);
+        dialog.show(getSupportFragmentManager(),"CarPickerDialog");
     }
 }
