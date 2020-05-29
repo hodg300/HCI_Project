57c690a5e97d9a134f45f0d5a21467251c1465a5
Sarel <sarelmicha93@gmail.com>
2020-05-29 10:37:26 +0300

    *Google maps has added to project

--- app/src/main/java/com/example/hci_project/MapActivity.java
+++ app/src/main/java/com/example/hci_project/MapActivity.java
@@ -1,21 +1,28 @@
 package com.example.hci_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
 
-    GoogleMap map;
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private GoogleMap map;
+    private ArrayList<LatLng> locations = new ArrayList<>();
+    private ArrayList<Marker> markers = new ArrayList<>();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +31,53 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        createLocations();
+    }
+
+    private void createLocations() {
+
+        locations.add(new LatLng(32.113611, 34.801954)); //aHug Hatzfoni
+        locations.add(new LatLng(32.1100635,34.843054)); //Segev Expresss
+        locations.add(new LatLng(32.0368665,34.9649833)); //Susu and Sons
+        locations.add(new LatLng(32.0938103,34.8110533)); //Tel Aviv musium
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         map = googleMap;
+        googleMap.setOnMarkerClickListener(this);
 
-        LatLng newYork = new LatLng(40.730610,-73.935242);
-        map.addMarker(new MarkerOptions().position(newYork).title("New York"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(newYork));
+        //aHug Hatzfoni
+        markers.add(map.addMarker(new MarkerOptions().position(locations.get(0)).title("החוג הצפוני")));
+        //Segev Expresss
+        markers.add(map.addMarker(new MarkerOptions().position(locations.get(1)).title("שגב אקספרס")));
+        //Susu and Sons
+        markers.add(map.addMarker(new MarkerOptions().position(locations.get(1)).title("סוסו אנד סאן")));
+        //Tel Aviv musium
+        markers.add(map.addMarker(new MarkerOptions().position(locations.get(1)).title("מוזיאון תל אביב")));
 
+
+        //Move Camera to Segev EXPRESS
+        map.moveCamera(CameraUpdateFactory.newLatLng(locations.get(1)));
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        if (markers.get(0).equals(marker)){
+
+        } else if(markers.get(1).equals(marker)){
+
+        } else if(markers.get(2).equals(marker)){
+
+        }else if(markers.get(3).equals(marker)){
+
+        }
+
+        return true;
     }
 }
